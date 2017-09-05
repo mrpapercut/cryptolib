db2207d2aae0aa8b6bdce8d4b3be09de519c9bd4
mrpapercut <mischa.rodermond@gmail.com>
2017-09-05 11:45:50 +0200

    sha1

--- test/CryptoLib.spec.js
+++ test/CryptoLib.spec.js
@@ -12,46 +12,23 @@ const CryptoLib = require(getFilePath('CryptoLib'));
 
 describe('CryptoLib', function() {
 	describe('Hashers', function() {
-		describe('MD5', function() {
-			it('should return correct hash', function() {
-				expect(CryptoLib.MD5('Hello world!').toString()).to.equal('86fb269d190d2c85f6e0468ceca42a20');
-			});
-		});
+		const hashes = {
+			MD5: '86fb269d190d2c85f6e0468ceca42a20',
+			RIPEMD160: '7f772647d88750add82d8e1a7a3e5c0902a346a3',
+			SHA1: 'd3486ae9136e7856bc42212385ea797094475802',
+			SHA224: '7e81ebe9e604a0c97fef0e4cfe71f9ba0ecba13332bde953ad1c66e4',
+			SHA256: 'c0535e4be2b79ffd93291305436bf889314e4a3faec05ecffcbb7df31ad9e51a',
+			SHA384: '86255fa2c36e4b30969eae17dc34c772cbebdfc58b58403900be87614eb1a34b8780263f255eb5e65ca9bbb8641cccfe',
+			SHA512: 'f6cde2a0f819314cdde55fc227d8d7dae3d28cc556222a0a8ad66d91ccad4aad6094f517a2182360c9aacf6a3dc323162cb6fd8cdffedb0fe038f55e85ffb5b6',
+			SHA3: '4010e792acb33c767f1bd0a49619769bf044eef2e8aca32717149dbc95e2b27ddc24aa8528c6a7f2a18b926c45d0aff290c18f1edfa04fce3ce91b7c69f3e5f5'
+		}
 
-		describe('RIPEMD-160', function() {
-			it('should return correct hash', function() {
-				expect(CryptoLib.RIPEMD160('Hello world!').toString()).to.equal('7f772647d88750add82d8e1a7a3e5c0902a346a3');
+		for (let hasher in hashes) {
+			describe(hasher, function() {
+				it('should match the precalculated hash', function() {
+					expect(CryptoLib[hasher]('Hello world!').toString()).to.equal(hashes[hasher]);
+				});
 			});
-		});
-
-		describe('SHA224', function() {
-			it('should return correct hash', function() {
-				expect(CryptoLib.SHA224('Hello world!').toString()).to.equal('7e81ebe9e604a0c97fef0e4cfe71f9ba0ecba13332bde953ad1c66e4');
-			});
-		});
-
-		describe('SHA256', function() {
-			it('should return correct hash', function() {
-				expect(CryptoLib.SHA256('Hello world!').toString()).to.equal('c0535e4be2b79ffd93291305436bf889314e4a3faec05ecffcbb7df31ad9e51a');
-			});
-		});
-
-		describe('SHA384', function() {
-			it('should return correct hash', function() {
-				expect(CryptoLib.SHA384('Hello world!').toString()).to.equal('86255fa2c36e4b30969eae17dc34c772cbebdfc58b58403900be87614eb1a34b8780263f255eb5e65ca9bbb8641cccfe');
-			});
-		});
-
-		describe('SHA512', function() {
-			it('should return correct hash', function() {
-				expect(CryptoLib.SHA512('Hello world!').toString()).to.equal('f6cde2a0f819314cdde55fc227d8d7dae3d28cc556222a0a8ad66d91ccad4aad6094f517a2182360c9aacf6a3dc323162cb6fd8cdffedb0fe038f55e85ffb5b6');
-			});
-		});
-
-		describe('SHA3-512', function() {
-			it('should return correct hash', function() {
-				expect(CryptoLib.SHA3('Hello world!').toString()).to.equal('4010e792acb33c767f1bd0a49619769bf044eef2e8aca32717149dbc95e2b27ddc24aa8528c6a7f2a18b926c45d0aff290c18f1edfa04fce3ce91b7c69f3e5f5');
-			});
-		});
+		}
 	});
 });
