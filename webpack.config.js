f0ea25a54cbe87e0d64d135353871ea94075aa2a
mrpapercut <mischa.rodermond@gmail.com>
2017-09-06 12:32:53 +0200

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        MD5: './entry/MD5'
+    },
+    module: {
+        loaders: [{
+            loader: 'babel-loader',
+            include: [path.resolve(__dirname, 'lib')],
+            query: {
+                presets: ['es2015']
+            }
+        }]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            filename: 'commons.js',
+            name: 'commons'
+        })
+    ]
+}
